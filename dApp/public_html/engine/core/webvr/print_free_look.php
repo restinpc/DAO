6c20474cf4e6d8a8309a688a2c0a9c6eb4fed341
Aleksandr V <devbyzero@yandex.ru>
2023-12-09 12:00:11 +0300

    1.0.1

--- dApp/public_html/engine/core/webvr/print_free_look.php
+++ dApp/public_html/engine/core/webvr/print_free_look.php
@@ -1,7 +1,7 @@
 <?php
 
 function print_free_look() {
-    return '<iframe id="free-look" src="https://nodes-tech.ru/apps/free-look" class="app" onLoad="loading_site();" width="100%"></iframe>
+    return '<iframe id="free-look" src="/apps/free-look" class="app" onLoad="loading_site();" width="100%"></iframe>
     <button class="vr icon" id="vr">
         <img id="icon vr_icon" src="/img/vr/vr.png" width="100%" onClick="document.panorama.vrMode();" /> 
     </button>
