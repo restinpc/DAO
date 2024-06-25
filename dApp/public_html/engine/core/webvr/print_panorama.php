0b8d165f45557afbabe81c10d52699120bb2bf32
Aleksandr V <devbyzero@yandex.ru>
2024-06-25 16:56:06 +0300

    1.0.3

--- dApp/public_html/engine/core/webvr/print_panorama.php
+++ dApp/public_html/engine/core/webvr/print_panorama.php
@@ -4,7 +4,7 @@ function print_panorama() {
     return '<iframe id="panorama" src="'.$_SERVER["DIR"].'/panorama.php?id=1#1.7188733853924727;167.87663397333137" class="app" onLoad="loading_site();"></iframe>
     <div id="map_frame">
         <div class="close_button close_wnd" style="position: fixed; top: 100px; z-index:3;" onclick=\'document.getElementById("map_frame").style.display = "none";\'>&nbsp;</div>
-        <iframe sandbox="allow-same-origin allow-top-navigation allow-forms allow-scripts" src="'.$_SERVER["DIR"].'/level.php?id=1" width=500 height=500></iframe>
+        <iframe sandbox="allow-same-origin allow-top-navigation allow-forms allow-scripts" style="zoom: 0.75" src="'.$_SERVER["DIR"].'/level.php?id=1" width=600 height=600></iframe>
     </div>
     <button class="map icon" id="map">
         <img id="map_icon" src="'.$_SERVER["DIR"].'/img/vr/map.png" width="100%" onClick="document.panorama.showMap();" /> 
