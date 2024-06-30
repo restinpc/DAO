a3a129105a76682f41a615f18e4958eb52b94acb
Aleksandr V <devbyzero@yandex.ru>
2024-06-30 15:52:09 +0300

    1.0.3

--- dApp/public_html/engine/code/uploader.php
+++ dApp/public_html/engine/code/uploader.php
@@ -219,7 +219,7 @@ if (!empty($_POST["name"])) {
                 <div id="image" draggable="false" onDragStart="return false;" style="background: url('.$_SERVER["DIR"].'/'.$f_name.') top left no-repeat; width: '.($width/ $scale).'px; height: '.($height/ $scale).'px; background-size: cover;" >
                     <img id="img" draggable="false" onDragStart="return false;" src="'.$_SERVER["DIR"].'/'.$f_name.'" style="width: '.($width/ $scale).'px; height: '.($height/ $scale).'px;" />
                 </div>
-                <div id="frame" draggable="false" onDragStart="return false;" style="width:'.($THUWIDTH/ $scale).'px; height:'.($THUHEIGHT/ $scale).'px;position: absolute;top: 28px;left: 28px;display:block;">
+                <div id="frame" draggable="false" onDragStart="return false;" style="width:'.($THUWIDTH/ $scale).'px; height:'.($THUHEIGHT/ $scale).'px;position: absolute;top: 0px;left: 0px;display:block;">
                     <table draggable="false" cellpadding=0 cellspacing=0 onDragStart="return false;" onMouseDown=\'if (document.uploader.dragMode != 3) { document.uploader.dragMode = 1; }\'>
                     <tr><td align=left valign=top></td></tr></table>
                     <div id="bottom_dot" onMouseDown=\'document.uploader.dragMode = 2;\' draggable="false" onDragStart="return false;">
