74501839e69229d1341794289f8dd0feb7dc2c95
Aleksandr V <devbyzero@yandex.ru>
2024-07-01 15:06:44 +0300

    1.0.3

--- dApp/public_html/engine/code/telegram.php
+++ dApp/public_html/engine/code/telegram.php
@@ -8,11 +8,23 @@
 * @license http://www.apache.org/licenses/LICENSE-2.0
 */
 
+require_once("engine/nodes/headers.php");
+require_once("engine/nodes/session.php");
+
+$query = 'SELECT value FROM `nodes_config` WHERE `name` = "telegram_url"';
+$res = engine::mysql($query);
+$data = mysqli_fetch_array($res);
+$url = $data["value"];
+$query = 'SELECT value FROM `nodes_config` WHERE `name` = "telegram_name"';
+$res = engine::mysql($query);
+$data = mysqli_fetch_array($res);
+$name = $data["value"];
+
 echo '<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Telegram: Contact @community_v3</title>
+    <title>Telegram: Contact @'.$url.'</title>
     <script>window.matchMedia&&window.matchMedia("(prefers-color-scheme: dark)").matches&&document.documentElement&&document.documentElement.classList&&document.documentElement.classList.add("theme_dark");</script>
     <link href="'.$_SERVER["PUBLIC_URL"].'/res/files/font-roboto.css?1" rel="stylesheet" type="text/css">
     <link href="'.$_SERVER["PUBLIC_URL"].'/res/files/bootstrap.min.css?3" rel="stylesheet">
@@ -27,20 +39,20 @@ echo '<!DOCTYPE html>
     <div class="tgme_body_wrap">
         <div class="tgme_page">
             <div class="tgme_page_photo">
-                <a href="tg://resolve?domain=community_v3"><img class="tgme_page_photo_image" src="./img/logo.png"></a>
+                <a href="tg://resolve?domain='.$url.'"><img class="tgme_page_photo_image" src="./img/logo.png"></a>
             </div>
             <div class="tgme_page_title" dir="auto">
-                <span dir="auto">Web 3.0 Community</span>
+                <span dir="auto">'.$name.'</span>
             </div>
             <div class="tgme_page_extra"></div>
             <div class="tgme_page_action">
-                <a class="tgme_action_button_new shine" target="_blank" href="https://t.me/community_v3">View in Telegram</a>
+                <a class="tgme_action_button_new shine" target="_blank" href="https://t.me/'.$url.'">View in Telegram</a>
             </div>
             <div class="tgme_page_action tgme_page_web_action">
-                <a class="tgme_action_button_new tgme_action_web_button" href="https://web.telegram.org/z/#?tgaddr=tg%3A%2F%2Fresolve%3Fdomain%3Dcommunity_v3" target="_blank"><span class="tgme_action_button_label">Open in Web</span></a>
+                <a class="tgme_action_button_new tgme_action_web_button" href="https://web.telegram.org/z/#?tgaddr=tg%3A%2F%2Fresolve%3Fdomain%3D'.$url.'" target="_blank"><span class="tgme_action_button_label">Open in Web</span></a>
             </div>
             <div class="tgme_page_additional">
-                If you have <strong>Telegram</strong>, you can view and join <br><strong>Web 3.0 Community</strong> right away.
+                If you have <strong>Telegram</strong>, you can view and join <br><strong>'.$name.'</strong> right away.
             </div>
         </div>
     </div>
@@ -48,7 +60,7 @@ echo '<!DOCTYPE html>
 <div id="tgme_frame_cont"></div>
 <script src="'.$_SERVER["PUBLIC_URL"].'/res/files/tgwallpaper.min.js?3"></script>
 <script type="text/javascript">
-    var protoUrl = "tg:\/\/resolve?domain=community_v3";
+    var protoUrl = "tg:\/\/resolve?domain='.$url.'";
     if (false) {
         var iframeContEl = document.getElementById("tgme_frame_cont") || document.body;
         var iframeEl = document.createElement("iframe");
